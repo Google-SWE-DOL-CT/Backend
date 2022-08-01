0f1a1b7a73ad7085353ec0d744242ac05f9ce9ce
CJGarciaMusic <CJGarciaMusic@gmail.com>
2022-08-01 16:18:32 -0400

    db url added

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const {Sequelize} = require('sequelize');
 const path = require('path');
+require('dotenv').config();
 
-const db = new Sequelize({
+const db = new Sequelize( process.env.DATABASE_URL || {
   dialect: 'sqlite',
   storage: path.join(__dirname, 'db.sqlite'),
   logging: false,
