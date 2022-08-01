563c0c5584310fb4a3ad576f2a4146111a93c41e
CJGarciaMusic <CJGarciaMusic@gmail.com>
2022-08-01 16:25:06 -0400

    config for db updated

--- db/db.js
+++ db/db.js
@@ -2,6 +2,8 @@ const {Sequelize} = require('sequelize');
 const path = require('path');
 require('dotenv').config();
 
+const config = {logging:false};
+
 if (process.env.DATABASE_URL) {
   config.dialectOptions = {
     ssl: {
