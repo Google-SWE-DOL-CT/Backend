b9aa34bb1018d278c3d9b5b162cace7284c20e0f
CJGarciaMusic <CJGarciaMusic@gmail.com>
2022-08-17 16:38:17 -0400

    put localhost into the access control all origin

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 
 const {requireToken, isAdmin} = require('./gatekeepingMW');
 
-const cors = require('cors');
+// const cors = require('cors');
 
 
 /* eslint-disable max-len */
@@ -11,17 +11,17 @@ const router = require('express').Router();
 
 module.exports = router;
 
-// router.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//   next();
-// });
+router.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
 
-router.use(cors({
-  credentials: true,
-}));
+// router.use(cors({
+//   credentials: true,
+// }));
 
 // router.get('/token', async (req, res) => {
 //   const options = {method: 'POST',
