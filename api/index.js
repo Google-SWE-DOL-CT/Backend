bd828973ddc8239ebf873a398f1c912fcc86e60a
CJGarciaMusic <CJGarciaMusic@gmail.com>
2022-08-17 16:26:22 -0400

    cors workaround

--- api/index.js
+++ api/index.js
@@ -9,7 +9,8 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
